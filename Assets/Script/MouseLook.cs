89921a0388d11ebf6c2e13b953c3a31b310c8556
CandyPusher <151382285+CandyPusher@users.noreply.github.com>
2026-01-21 11:53:18 +0100

    mouse look added

---
+++ Assets/Script/MouseLook.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivy = 100f;
+    public Transform playerBody;
+    float xRotation;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;//cursore è bloccato al centro dello schermo
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivy * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivy * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
